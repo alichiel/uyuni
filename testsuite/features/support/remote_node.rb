f61711119e571d4120dca1c8ce4c35aeccfd03eb
alichiel@proton.me <ales.marecek@suse.com>
2026-07-10 17:42:23 +0200

    Revert "proxy debug +1"
    Being reworked.

--- testsuite/features/support/remote_node.rb
+++ testsuite/features/support/remote_node.rb
@@ -197,17 +197,15 @@ class RemoteNode
 
   # TMP method for DEBUGGING
   def proxy_run_until_ok(cmd, timeout: 360, runs_in_container: true)
-    repeat_until_timeout(timeout: 360, report_result: true) do
+    repeat_until_timeout(timeout: timeout, report_result: true) do
       result, code = run(cmd, check_errors: false, runs_in_container: runs_in_container)
       return [result, code] if code.zero?
       sleep 2
     end
+    puts "TIMED OUT!"
     run("mgrpxy restart", check_errors: false, runs_in_container: false)
     sleep 60
     result, code = run(cmd, check_errors: false, runs_in_container: runs_in_container)
-    run("echo '### TIMED OUT ###' >>/tmp/proxy_debug.log", check_errors: false, runs_in_container: false)
-    run("echo '  RESULT: #{result}' >>/tmp/proxy_debug.log", check_errors: false, runs_in_container: false)
-    run("echo '  CODE: #{code}' >>/tmp/proxy_debug.log", check_errors: false, runs_in_container: false)
     return [result, code] if code.zero?
     result
   end
